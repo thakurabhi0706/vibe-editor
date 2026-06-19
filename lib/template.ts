8a640fd04b0081bf46ca6a746f439076e9a4eff6
thakurabhhi0706 <abhishekkumar.singh070604@gmail.com>
2026-06-19 07:10:08 +0530

    File Explorer Added

---
+++ lib/template.ts
@@ -0,0 +1,8 @@
+export const templatePaths = {
+  REACT: "/starters/react-ts",
+  NEXTJS: "/starters/nextjs-new",
+  EXPRESS: "/starters/express-simple",
+  VUE: "/starters/vue",
+  HONO: "/starters/hono-nodejs-starter",
+  ANGULAR: "/starters/angular",
+};
